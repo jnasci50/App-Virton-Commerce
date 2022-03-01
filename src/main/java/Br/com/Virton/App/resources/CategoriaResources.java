6c1c7a928ba4797077db57ae8323050ed02ae2db
Jairo pereira nascimento <jairoarea1@hotmail.com>
2022-03-01 18:43:43 -0300

    Criação clase categorias

--- src/main/java/Br/com/Virton/App/resources/CategoriaResources.java
+++ src/main/java/Br/com/Virton/App/resources/CategoriaResources.java
@@ -1,17 +1,31 @@
 package Br.com.Virton.App.resources;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import Br.com.Virton.App.domain.Categoria;
 
 @RestController
 @RequestMapping(value ="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET )
-	public String Mensagem() {
-		return "App Virton está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<>();
+		
+		listaCategorias.add(categoria1);
+		listaCategorias.add(categoria2);
+		
+		return listaCategorias ;
 		
 	}
 }

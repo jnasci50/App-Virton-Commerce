de7369135aa64b2253887437e9ba3ee5d178a685
Jairo pereira nascimento <jairoarea1@hotmail.com>
2022-02-28 21:51:47 -0300

    Projeto Criado

---
+++ src/main/java/Br/com/Virton/App/AppVirtonCommerceApplication.java
@@ -0,0 +1,13 @@
+package Br.com.Virton.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVirtonCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVirtonCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Br/com/Virton/App/AppVirtonCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package Br.com.Virton.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVirtonCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
